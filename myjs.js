bc1beb52364fb00f1663ff6d748f7c82c78d9890
mallorinapg <mallorina.mp@gmail.com>
2021-11-07 18:58:03 -0500

    installed hamburger button, added notes, adjusted css

--- myjs.js
+++ myjs.js
@@ -1,3 +1,4 @@
+/* Contact Form JS */
 function getData() {
     let allMsg = [];
     let first_name = document.contactMeForm.firstname.value;
@@ -20,3 +21,22 @@ function preview() {
     previewMsg.innerHTML = `${data}`;
     document.contactMeForm.reset();
 }
+
+/* Hamburger Btn Animation */
+const menuBtn = document.querySelector('.menu-btn');
+const nav = document.querySelector('.navMenu');
+let menuOpen = false;
+menuBtn.addEventListener('click', () => {
+    if (!menuOpen) {
+        menuBtn.classList.add('open');
+        menuOpen = true;
+        nav.classList.remove('navMenu');
+        nav.classList.add('active');
+
+    } else {
+        menuBtn.classList.remove('open');
+        menuOpen = false;
+        nav.classList.remove('active');
+        nav.classList.add('navMenu');
+    }
+});
