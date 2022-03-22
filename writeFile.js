f630bb19d28251b8e7084e73ac414ea550f9066a
mallorinapg <mallorina.mp@gmail.com>
2022-03-21 23:10:33 -0400

    updated packages, shopping cart, calculate js

--- writeFile.js
+++
@@ -1,8 +0,0 @@
-function writeToFile() {
-    let oForm = form.name.value;
-    let display = document.getElementById("demo");
-    display.innerHTML = oForm;
-}
-FileSystem.writeFile('user.Info.txt,'){
-    
-}
